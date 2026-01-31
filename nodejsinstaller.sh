1d1fa681d9503f823cb1e0176f63827027afb7a6
Luky <spamkinder6@gmail.com>
2026-01-31 22:52:12 +0100

    Add Node.js installation script
    Download and install Node.js version 24.13.0, update PATH.

--- nodejsinstaller.sh
+++ nodejsinstaller.sh
@@ -1,1 +1,15 @@
 wget https://nodejs.org/dist/v24.13.0/node-v24.13.0-linux-x64.tar.xz
+mv node-* ~/
+cd ..
+tar -xf node-*
+rm node-v24.13.0-linux-x64.tar.xz
+sudo mv node-v24.13.0-linux-x64 /usr/local/node
+echo 'export PATH=/usr/local/node/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
+echo
+echo
+echo
+echo
+echo
+node -v
+npm -v
